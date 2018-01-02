ecd063ea0bda64809a632601b0a44264f7260def
Randy Burgess <wrburgess@gmail.com>
2018-01-01 21:52:37 -0600

    WIP, images, fonts, favicons

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,16 +1,27 @@
 //= require jquery
 //= require mediaelement
 
-$('.post-index li').click(function(evnt) {
-  $('.post-index li').removeClass('active');
-  $('.post-show .post').removeClass('active');
+$('.posts h3').click(function(evnt) {
+  // deactivate all post list items and post sections
+  $('h3').removeClass('active');
+  $('.post-body').removeClass('active');
 
-  const $li = $(evnt.target);
-  const postId = $li.data('id');
-  const $post = $('.post[data-id="' + postId + '"]');
+  // get elements and data points
+  const $mediaPlayerTitle = $('.media-player h3');
+  const $mediaPlayerAudio = $('.media-player audio');
+  const $h3 = $(evnt.target);
+  const postId = $h3.data('id');
+  const postFileUrl = $h3.data('file-src');
+  const postTitle = $h3.text();
+  const $post = $('.post-body[data-id="' + postId + '"]');
 
-  $li.addClass('active');
+  // activate post list item and post section
+  $h3.addClass('active');
   $post.addClass('active');
+
+  // update media player title and file source
+  $mediaPlayerTitle.text(postTitle)
+  $mediaPlayerAudio.attr('src', postFileUrl)
 });
 
 $('audio').mediaelementplayer({
