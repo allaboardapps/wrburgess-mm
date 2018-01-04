bd9a5a403bebff3766915a3432f62e65598b3cc5
Randy Burgess <wrburgess@gmail.com>
2018-01-03 20:08:48 -0600

    allowing ep toggle

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -2,26 +2,34 @@
 //= require mediaelement
 
 $('.episodes h3').click(function(evnt) {
-  // deactivate all post episode items and episode sections
-  $('h3').removeClass('active');
-  $('.episode-body').removeClass('active');
-
-  // get elements and data points
-  const $mediaPlayerTitle = $('.media-player h3');
-  const $mediaPlayerAudio = $('.media-player audio');
   const $h3 = $(evnt.target);
-  const episodeId = $h3.data('id');
-  const episodeFileUrl = $h3.data('file-src');
-  const episodeTitle = $h3.text();
-  const $episode = $('.episode-body[data-id="' + episodeId + '"]');
 
-  // activate episode list item and episode section
-  $h3.addClass('active');
-  $episode.addClass('active');
+  if ($h3.hasClass('active')) {
+    // deactivate all post episode items and episode sections
+    $('h3').removeClass('active');
+    $('.episode-body').removeClass('active');
+  } else {
+    // deactivate all post episode items and episode sections
+    $('h3').removeClass('active');
+    $('.episode-body').removeClass('active');
 
-  // update media player title and file source
-  $mediaPlayerTitle.text(episodeTitle)
-  $mediaPlayerAudio.attr('src', episodeFileUrl)
+    // get elements and data points
+    const $mediaPlayerTitle = $('.media-player h3');
+    const $mediaPlayerAudio = $('.media-player audio');
+    const $h3 = $(evnt.target);
+    const episodeId = $h3.data('id');
+    const episodeFileUrl = $h3.data('file-src');
+    const episodeTitle = $h3.text();
+    const $episode = $('.episode-body[data-id="' + episodeId + '"]');
+
+    // activate episode list item and episode section
+    $h3.addClass('active');
+    $episode.addClass('active');
+
+    // update media player title and file source
+    $mediaPlayerTitle.text(episodeTitle)
+    $mediaPlayerAudio.attr('src', episodeFileUrl)
+  }
 });
 
 $('audio').mediaelementplayer({
