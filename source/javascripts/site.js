b93004c18350eb6d6e8279bf5d13a4855146cc74
Randy Burgess <wrburgess@gmail.com>
2018-02-05 14:15:41 -0600

    updating blog post

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,4 +1,11 @@
 //= require jquery
 //= require vanilla-lazyload
+//= require highlight
 
 new LazyLoad();
+
+$(document).ready(function() {
+  $('code').each(function(i, block) {
+    hljs.highlightBlock(block);
+  });
+});
